70f5c8ae2e1aa9c2031f0b9c10036c08a35e972b
ntguilty <jura.marushenko@gmail.com>
2018-11-02 10:22:25 +0000

    Created initial files

---
+++ js/index.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.digits, .operation')
+  .forEach( el => el.addEventListener('click', digitOperClick ) );
+function digitOperClick(e) {
+  const target = e.target;
+  const display = document.querySelector('.display');
+  display.value += target.innerText;
+}
+
+document.querySelector('.equal') 
+  .addEventListener('click', equal);
+  
+  function equal() {
+  const display = document.querySelector('.display');
+  display.value = eval(display.value);
+}
+
+document.querySelector('.reset') 
+  .addEventListener('click', reset);
+  
+  function reset() {
+  const display = document.querySelector('.display');
+  display.value = '';
+}
+
+
